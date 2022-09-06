41edfd44b3e5e62685e0719b6a9936e410c033b5
itheima <itheima@itcast.cn>
2022-09-06 11:20:30 +0800

    www

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,17 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div>111</div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+//   git log查看commit记录
+// git status查看当前仓库的状态
+// git add·提交到暂存区
+// git commit -m
+// -feat:新增特性
+// style:样式的修改
+// fixed: debug
 
-nav {
-  padding: 30px;
+export default {}
+</script>
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style></style>
